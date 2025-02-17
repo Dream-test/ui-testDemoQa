1247c97e74b5e917932e55c95193e520383b22de
Dream-test <stsiapkou@gmail.com>
2025-02-17 18:46:30 +0100

    connect prometheus & grafana

--- src/test/java/uiTest/DemoQaTest.java
+++ src/test/java/uiTest/DemoQaTest.java
@@ -2,11 +2,16 @@ package uiTest;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
+import io.prometheus.client.CollectorRegistry;
+import io.prometheus.client.Counter;
+import io.prometheus.client.exporter.PushGateway;
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -15,37 +20,65 @@ import java.util.Map;
 import static com.codeborne.selenide.Configuration.baseUrl;
 import static com.codeborne.selenide.Selenide.*;
 
+@ExtendWith(ExecutionWatcher.class)
 public class DemoQaTest {
     private final Locators locators = new Locators();
     private static WebDriver driver;
 
+    static CollectorRegistry registry;
+    static Counter requests;
+    public static Counter failedRequests;
+    public static Counter passedRequests;
+
     private static void cofigurationRemote() throws URISyntaxException, MalformedURLException {
         baseUrl = "https://demoqa.com";
         //Configuration.browserSize = "1920x1080";
+        String startRemote = System.getenv("StartRemote");
+        String selenoidUri = System.getenv("SELENOID_URI");
 
-        if (System.getenv("StartRemote").equals("yes")) {
+        if ("yes".equalsIgnoreCase(startRemote) && selenoidUri != null) {
             Configuration.remote = System.getenv("SELENOID_URI");
             Configuration.browser = "chrome";
+
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--no-sandbox");
+            options.addArguments("--ignore-certificate-errors");
+            options.addArguments("--window-size=1920,1080");
+            //options.addArguments("--force-device-scale-factor=0.5");
+
+            options.setCapability("browserName", "chrome");
+            options.setCapability("acceptInsecureCerts", true);
+            options.setCapability("selenoid:options", Map.of("enableVNC", true));
+
+            assert Configuration.remote != null;
+            URI selenoidURI = new URI(Configuration.remote);
+            driver = new RemoteWebDriver(selenoidURI.toURL(), options);
+        } else {
+            Configuration.browser = "chrome";
+            Configuration.browserSize = "1366x768";
+            Configuration.headless = false;
+            Configuration.timeout = 1000;
         }
-
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--no-sandbox");
-        options.addArguments("--ignore-certificate-errors");
-        options.addArguments("--window-size=1920,1080");
-        //options.addArguments("--force-device-scale-factor=0.5");
-
-        options.setCapability("browserName", "chrome");
-        options.setCapability("acceptInsecureCerts", true);
-        options.setCapability("selenoid:options", Map.of("enableVNC", true));
-
-        assert Configuration.remote != null;
-        URI selenoidURI = new URI(Configuration.remote);
-        driver = new RemoteWebDriver(selenoidURI.toURL(), options);
     }
 
     @BeforeAll
     public static void setUp() throws MalformedURLException, URISyntaxException {
         cofigurationRemote();
+
+        registry = new CollectorRegistry();
+        requests = Counter.build()
+                .name("total_tests")
+                .help("Number of tests run")
+                .register(registry);
+        failedRequests = Counter.build()
+                .name("failed_tests")
+                .help("Number of failed tests")
+                .register(registry);
+        passedRequests = Counter.build()
+                .name("passed_tests")
+                .help("Number of passed tests")
+                .register(registry);
+
        // baseUrl = "https://demoqa.com";
        // Configuration.browserSize = "1366x768";
        // Configuration.headless = false;
@@ -65,9 +98,21 @@ public class DemoQaTest {
         sessionStorage().clear();
     }
 
+    @AfterEach
+    public void testTearDown(TestInfo testInfo) {
+        requests.inc();
+        if (testInfo.getTags().contains("failed")) {
+            failedRequests.inc();
+        } else {
+            passedRequests.inc();
+        }
+    }
+
     @AfterAll
-    public static void tearDownAll() {
+    public static void tearDownAll() throws IOException {
         closeWebDriver();
+        PushGateway pg = new PushGateway("127.0.0.1:9091");
+        pg.push(registry, "my_batch_job");
     }
 
 
